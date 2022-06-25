12af04ca0f719466422403a2b66c02a998c0490a
lZarzao <luiszarzao@icloud.com>
2022-06-25 15:56:06 -0400

    temporal search-bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+const api = {
+  key: "2abd254968449e3f2aca30a513bc1f53",
+  base: "https://api.openweathermap.org/data/3.0"
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."/>
+        </div>
+      </main>
     </div>
   );
 }
