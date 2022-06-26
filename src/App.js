035bd5a143ac048241ac7d27a5ddb4cdf302097e
lZarzao <luiszarzao@icloud.com>
2022-06-25 23:48:11 -0400

    first front idea

--- src/App.js
+++ src/App.js
@@ -5,12 +5,41 @@ const api = {
   base: "https://api.openweathermap.org/data/3.0"
 }
 
+const dateBuilder = (d) => {
+  console.log(d)
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`
+}
+
 function App() {
   return (
     <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..."/>
+        </div>
+        <div className="location-info">
+          <div className="location">
+            <p>Asunción, Py</p>
+          </div>
+          <div className="date">
+            <p>{dateBuilder(new Date())}</p>
+          </div>
+        </div>
+        <div className="weather-info">
+          <div className="temp">
+            <h1>26°C</h1>
+          </div>
+          <div className="weather">
+            cloudy
+          </div>
         </div>
       </main>
     </div>
