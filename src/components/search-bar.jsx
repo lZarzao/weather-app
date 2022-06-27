dbeedcc3cfedb0f0745c42a768c3dfaba60d559d
lZarzao <luiszarzao@icloud.com>
2022-06-27 11:12:54 -0400

    geolocation button

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -7,7 +7,22 @@ function SearchBar({setQuery, units, setUnits}) {
 
   const handleSearchClick = (id) => {
     if (city !== "") setQuery({q: city})
+    changeSelector(id)
+  }
 
+  const handleLocationClick = (id) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        setQuery({ lat, lon})
+        changeSelector(id)
+      })
+    }
+  }
+
+  const changeSelector = (id) => {
     let element = document.getElementById(id);
     let belement = document.getElementsByClassName("selected")[0];
     belement && belement.classList.remove("selected")
@@ -18,7 +33,7 @@ function SearchBar({setQuery, units, setUnits}) {
     <div className="search-box">
           <input value={city} onChange={(e) => setCity(e.currentTarget.value)} type="text" className="search-bar" placeholder="search..."/>
           <UilSearch id="search-icon" size={24} className="search-icon" onClick={() => handleSearchClick("search-icon")}/>
-          <UilLocationPoint size={24} className="search-icon" />
+          <UilLocationPoint id="point-icon" size={24} className="search-icon" onClick={() => handleLocationClick("point-icon")} />
           <div className="weather-unit">
             <button id="celcius" name="celcius" className="selected-unit">°C</button>
             <p>|</p>
